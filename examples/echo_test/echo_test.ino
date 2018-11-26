605438336843b084d67ca68a6338f57613423f3b
Antonio C. Domínguez Brito <antonio.dominguez@ulpgc.es>
2018-11-26 00:01:09 +0000

    New version 3.0.
    This version is an improvement in order to solve a bug and a deviation from "good behavior":
    
    1. TC interrupts are programmed at a quarter of the bit time period. This avoid a problems at receiving bytes starting at the very end of the half bit time period in the previous version.
    2. In the previous version stop bit duration was not guaranteed when finishing a transmission, the second change to the library guarantees this exxtend.
    
    Thanks to David Hansel (david@hansels.net) for diagnosing those two problems, and for suggesting alternative solutions for solving them.

---
+++ examples/echo_test/echo_test.ino
@@ -0,0 +1,70 @@
+/**
+ ** soft_uart library
+ ** Copyright (C) 2015-2018
+ **
+ **   Antonio C. Domínguez Brito <antonio.dominguez@ulpgc.es>
+ **     División de Robótica y Oceanografía Computacional <www.roc.siani.es>
+ **     and Departamento de Informática y Sistemas <www.dis.ulpgc.es>
+ **     Universidad de Las Palmas de Gran  Canaria (ULPGC) <www.ulpgc.es>
+ **  
+ ** This file is part of the soft_uart library.
+ ** The soft_uart library is free software: you can redistribute it and/or modify
+ ** it under  the  terms of  the GNU  General  Public  License  as  published  by
+ ** the  Free Software Foundation, either  version  3  of  the  License,  or  any
+ ** later version.
+ ** 
+ ** The  soft_uart library is distributed in the hope that  it  will  be  useful,
+ ** but   WITHOUT   ANY WARRANTY;   without   even   the  implied   warranty   of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE.  See  the  GNU  General
+ ** Public License for more details.
+ ** 
+ ** You should have received a copy  (COPYING file) of  the  GNU  General  Public
+ ** License along with the soft_uart library.
+ ** If not, see: <http://www.gnu.org/licenses/>.
+ **/
+/*
+ * File: echo_test.ino 
+ * Description: This is an echo program for testing the library with another
+ * device (another microcontroller, a computer, etc.).
+ * Date: November 25th, 2018
+ * Author: Antonio C. Dominguez-Brito <antonio.dominguez@ulpgc.es>
+ * ROC-SIANI - Universidad de Las Palmas de Gran Canaria - Spain
+ */
+
+#include "soft_uart.h"
+
+using namespace arduino_due;
+
+#define RX_PIN 10 // software serial port's reception pin
+#define TX_PIN 11 // software serial port's transmision pin
+#define SOFT_UART_BIT_RATE 57600 // 57600 38400 1200 19200 9600 115200 300
+#define RX_BUF_LENGTH 256 // software serial port's reception buffer length
+#define TX_BUF_LENGTH 256 // software serial port's transmision buffer length
+
+// declaration of software serial port object serial_tc4
+// which uses timer/counter channel TC4
+serial_tc4_declaration(RX_BUF_LENGTH,TX_BUF_LENGTH);
+auto& serial_obj=serial_tc4; // serial_tc4_t& serial_obj=serial_tc4;
+
+void setup()
+{
+  // serial_obj initialization
+  serial_obj.begin(
+    RX_PIN,
+    TX_PIN,
+    SOFT_UART_BIT_RATE,
+    soft_uart::data_bit_codes::EIGHT_BITS,
+    soft_uart::parity_codes::NO_PARITY,
+    soft_uart::stop_bit_codes::ONE_STOP_BIT
+  );
+}
+
+void loop() 
+{
+  if(serial_obj.available())
+  {
+    auto data=serial_obj.read();
+    if(data>=0) 
+      serial_obj.write(static_cast<uint8_t>(data&0xff));
+}
+
